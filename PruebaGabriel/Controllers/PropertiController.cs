de0735122eea9a1fc34985a578382f5dc6e2d12f
GabrielManchola <50031687+GabrielManchola@users.noreply.github.com>
2022-05-16 16:13:39 -0500

    api terminada

--- PruebaGabriel/Controllers/PropertiController.cs
+++ PruebaGabriel/Controllers/PropertiController.cs
@@ -8,22 +8,72 @@ namespace PruebaGabriel.Controllers
     [ApiController]
     public class PropertiController : ControllerBase
     {
-        private static List<PropertyModel> Propertys = new List<PropertyModel>()
-       {
-                new PropertyModel
-                {
-                    IdImagenProperty = 1,
-                    IdOwner = 1,
-                    Adress = "Funciona",
-                    Price = 123
-                }
-        };
+        private readonly DataContext context;
+
+        public PropertiController(DataContext context)
+        {
+            this.context = context;
+        }
 
 
         [HttpGet]
         public async Task<ActionResult<List<PropertyModel>>> Get()
         {
-            return Ok(Propertys);
+            return Ok(await context.Property.ToListAsync());
         }
+
+        [HttpPost]
+        public async Task<ActionResult<List<PropertyModel>>> AddProperty(PropertyModel modelo)
+        {
+            context.Property.Add(modelo);
+            await context.SaveChangesAsync();
+
+            return Ok(await context.Property.ToListAsync());
+        }
+
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<PropertyModel>> GetId(int id)
+        {
+            var property = await context.Property.FindAsync(id);
+            if (property == null)
+            {
+                return BadRequest("Property not found.");
+            }
+            return Ok(property);
+        }
+
+
+        [HttpPut]
+        public async Task<ActionResult<List<PropertyModel>>> UpdatePrice(PropertyModel modelo)
+        {
+            var dboproperty = await context.Property.FindAsync(modelo.IdProperty);
+            if (dboproperty == null)
+            {
+                return BadRequest("Property not found.");
+            }
+
+           
+            dboproperty.Price = modelo.Price;
+
+            await context.SaveChangesAsync();
+            return Ok(await context.Property.ToListAsync());
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<List<PropertyModel>>> Delete(int id)
+        {
+            var owner = await context.Property.FindAsync(id);
+            if (owner == null)
+            {
+                return BadRequest("Property not found.");
+            }
+
+            context.Property.Remove(owner);
+            await context.SaveChangesAsync();
+            return Ok(await context.Property.ToListAsync());
+        }
+
+
     }
 }
